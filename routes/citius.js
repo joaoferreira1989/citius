967f50e79e2fd49fda65e6173c68456753066d92
Joao Ferreira <Joao.Ferreira@blip.pt>
2018-03-03 18:49:44 +0000

    Paralelize different date requests

--- routes/citius.js
+++ routes/citius.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { doFetch } = require('../lib/req/citius');
 
 router.get('/', function (req, res, next) {
-    const initialDate = '02-01-2018';
-    const finalDate = '02-01-2018';
+    const initialDate = '10-01-2018';
+    const finalDate = '10-01-2018';
 
     doFetch(initialDate, finalDate).then(() => {
         res.send('fetching done');
