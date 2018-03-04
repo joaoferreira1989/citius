2a01cfdb3b57d96f62019af603a8d686929449c4
Joao Ferreira <Joao.Ferreira@blip.pt>
2018-03-04 15:57:46 +0000

    Export results to excel

--- routes/citius.js
+++ routes/citius.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { PEOPLE_TYPES } = require('../lib/tools/constants');
 const { doFetch } = require('../lib/req/citius');
+const { exportToExcel } = require('../lib/exporter/csv');
 
 router.get('/', function (req, res, next) {
     const initialDate = '03-01-2018';
     const finalDate = '03-01-2018';
 
     doFetch(initialDate, finalDate).then((response) => {
-        console.log(response);
-        res.send('fetching done');
+        const csvData = response.map((act) => {
+            return act.map(({ process, people }) => {
+                const admin = people.find((person) => {
+                    return person.type === PEOPLE_TYPES.ADMINISTRADOR_INSOLVENCIA;
+                });
+
+                return {
+                    processNumber: process.processDetails.number,
+                    date: process.date,
+                    act: process.act,
+                    court: process.court,
+                    judgement: process.processDetails.judgement,
+                    admin: admin.name,
+                    adminNif: admin.nif
+                };
+            });
+        });
+        var date = new Date();
+        const timestamp = date.getTime();
+        const report = exportToExcel(csvData);
+
+        res.attachment('report' + timestamp + '.xlsx');
+        return res.send(report);
     });
 });
 
