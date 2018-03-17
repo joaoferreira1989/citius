edfb6dc409b043ad33f5ccf7bd0cca1046e89d72
Joao Ferreira <Joao.Ferreira@blip.pt>
2018-03-17 02:26:48 +0000

    Add date range datepicker

---
+++ public/javascripts/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    let startDate = new Date();
+    let endDate = new Date();
+
+    $('#date-range').daterangepicker(
+        {
+            locale: {
+                format: 'DD-MM-YYYY'
+            },
+            todayHighlight: true,
+            endDate: moment(),
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+             }
+        },
+        function (start, end, label) {
+            startDate = start.format('DD-MM-YYYY');
+            endDate = end.format('DD-MM-YYYY');
+
+            console.log(startDate + ' to ' + endDate);
+        }
+    );
+});
