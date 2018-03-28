cda48f2defc231c641f2ae04ee19e8155f53493f
Joao Ferreira <Joao.Ferreira@blip.pt>
2018-03-28 23:22:18 +0100

    Create tables schema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var citius = require('./routes/citius');
+var importer = require('./routes/importer');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', index);
 app.use('/citius', citius);
+app.use('/importer', importer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
