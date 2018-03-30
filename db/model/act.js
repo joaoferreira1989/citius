4e04783cb350b1a5de957a20000edce66f6f21d8
Joao Ferreira <Joao.Ferreira@blip.pt>
2018-03-30 16:07:56 +0100

    Insert data synchronously on DB

--- db/model/act.js
+++ db/model/act.js
@@ -4,10 +4,8 @@ function getActIdByName(connection, name) {
             if (error) { return reject(error); }
 
             if (rows.length > 0) {
-                console.log('found act', rows);
                 resolve(rows[0].id);
             } else {
-                console.log('not found act', rows);
                 addAct(connection, name)
                     .then((insertId) => {
                         resolve(insertId);
--- routes/importer.js
+++ routes/importer.js
@@ -2,30 +2,46 @@ const express = require('express');
 const router = express.Router();
 const { PEOPLE_TYPES } = require('../lib/tools/constants');
 const { doFetch } = require('../lib/req/citius');
-const { writeToFile } = require('../lib/tools/fs-helper');
 const { insertProcess } = require('../db/model/process');
 
 router.get('/', function (req, res, next) {
     const initialDate = '01-02-2018';
-    const finalDate = '01-02-2018';
+    const finalDate = '02-02-2018';
 
     doFetch(initialDate, finalDate).then((response) => {
-        const csvData = response.map((act) => {
-            return act.map(({ process, people }) => {
-                insertProcess(process)
-                    .then((processId) => {
-                        console.log('Process ' + processId + ' inserted!');
-                    })
-                    .catch((error) => {
-                        console.log('Error inserting process: ' + process.processDetails.number + ' - ' + process.date, error.sqlMessage);
-                    });
+        const processList = response.reduce((acc, act) => {
+            act.forEach(({ process, people }) => {
+                acc.push(process);
             });
-        });
 
-        //writeToFile(JSON.stringify(response, null, 4));
+            return acc;
+        }, []);
+
+        insertProcessList(processList);
 
         return res.send('done');
     });
 });
+
+function insertProcessList(processList) {
+    let insertPromise = Promise.resolve();
+
+    for (let i = 0; i < processList.length; i++) {
+        insertPromise = insertPromise.then(() => {
+            let process = processList[i];
+
+            return insertProcess(process)
+                .then((processId) => {
+                    console.log('Process ' + processId + ' inserted!');
+                })
+                .catch((error) => {
+                    const processNr = process && process.processDetails && process.processDetails.number;
+                    const errorMsg = error && error.sqlMessage;
+
+                    console.log('Error inserting process: ' + processNr + ' - ' + process.date, errorMsg);
+                });
+        });
+    }
+}
 
 module.exports = router;
